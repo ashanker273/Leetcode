36a00780a339c5f7c0faa9a248c43b8624da7c1e
avinashshanker <savinash27394@gmail.com>
2025-02-10 18:01:25 -0600

    solutionTwoSum

---
+++ src/main/java/org/apple/leetcode/solutionTwoSum.java
@@ -0,0 +1,19 @@
+package org.apple.leetcode;
+
+import java.util.HashMap;
+
+public class solutionTwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        //iterate through all the values in nums integer array
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(target - nums[i])) {
+                map.put(nums[i], i);
+            } else {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
