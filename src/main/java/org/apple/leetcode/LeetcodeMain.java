49a06a3fe1cdcf6d0f372ec2d5b2e3c16cec65ef
avinashshanker <savinash27394@gmail.com>
2025-02-13 21:54:06 -0600

    isPalindrome

--- src/main/java/org/apple/leetcode/LeetcodeMain.java
+++ src/main/java/org/apple/leetcode/LeetcodeMain.java
@@ -5,5 +5,9 @@ public class LeetcodeMain {
         solutionTwoSum solutionTwoSum = new solutionTwoSum();
         System.out.println(solutionTwoSum.twoSum(new int[]{2, 7, 11, 15}, 9));
 
+        solutionIsPalindrome solutionisPalindromes = new solutionIsPalindrome();
+
+        System.out.println(solutionisPalindromes.isPalindrome("A man, a plan, a canal: Panama"));
+
     }
 }
