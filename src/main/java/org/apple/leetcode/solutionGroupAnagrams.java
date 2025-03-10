4f965b8bc8ff6a2ee4baacb07839f14834af1269
avinashshanker <savinash27394@gmail.com>
2025-03-10 11:40:44 -0500

    Starting with udemy

--- src/main/java/org/apple/leetcode/solutionGroupAnagrams.java
+++ src/main/java/org/apple/leetcode/solutionGroupAnagrams.java
@@ -6,21 +6,37 @@ import java.util.HashMap;
 import java.util.List;
 
 public class solutionGroupAnagrams {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
+//    public List<List<String>> groupAnagrams(String[] strs) {
+//        HashMap<String, List<String>> map = new HashMap<>();
+//
+//        for (String str : strs) {
+//            String sortedStr = sortString(str);
+//            map.putIfAbsent(sortedStr, new ArrayList<>());
+//            map.get(sortedStr).add(str);
+//        }
+//
+//        return new ArrayList<>(map.values());
+//    }
+//    //better way
+//    private String sortString(String str) {
+//        char[] arr = str.toCharArray();
+//        Arrays.sort(arr);
+//        return new String(arr);
+//    }
 
-        for (String str : strs) {
-            String sortedStr = sortString(str);
-            map.putIfAbsent(sortedStr, new ArrayList<>());
-            map.get(sortedStr).add(str);
+
+        public List<List<String>> groupAnagrams(String[] strs) {
+            HashMap<String, List<String>> map = new HashMap<>();
+
+            for(String str : strs){
+                String sortedStr = Arrays.toString(Arrays.sort(str.toCharArray()));
+
+                map.putIfAbsent(sortedStr, new ArrayList<String>());
+                map.get(sortedStr).add(str);
+            }
+
+            return new ArrayList<>(map.values());
         }
+        
 
-        return new ArrayList<>(map.values());
-    }
-    //better way
-    private String sortString(String str) {
-        char[] arr = str.toCharArray();
-        Arrays.sort(arr);
-        return new String(arr);
-    }
 }
